9b96a38e87e959f3398939cad7e8201503bf10d0
Kenta Iwami <g2117009@fun.ac.jp>
2018-08-11 01:10:39 +0900

    ファイル書き込みを実装

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import os
+import datetime
 
 app = Flask(__name__)
 
@@ -9,7 +11,23 @@ def detect_noise():
     sensor = request.args.get('sensor')
     print(z, sensor)
 
+    write_log_file(sensor, z)
     return jsonify({'z': z, 'sensor': sensor})
+
+
+def write_log_file(sensor, z):
+    os.makedirs('log', exist_ok=True)
+
+    now = datetime.datetime.now()
+    now_str = now.strftime("%Y/%m/%d %H:%M:%S")
+    file_path = 'log/{}.csv'.format(sensor)
+
+    file = open(file_path, 'a')
+
+    if len(open(file_path).readlines()) == 0:
+        file.write('{},{},{}'.format(now_str, sensor, z))
+
+    file.close()
 
 
 if __name__ == '__main__':
