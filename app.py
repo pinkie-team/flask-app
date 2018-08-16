beb9bed910cd38331a464d22d85c50485ceb6d03
Kenta Iwami <g2117009@fun.ac.jp>
2018-08-16 17:30:33 +0900

    effectを設定するエンドポイントを実装

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 import os
 import datetime
+import socket
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def detect_noise():
     z = request.args.get('z')
     sensor = request.args.get('sensor')
@@ -13,6 +14,30 @@ def detect_noise():
 
     write_log_file(sensor, z)
     return jsonify({'z': z, 'sensor': sensor})
+
+
+@app.route('/effect/<effect_id>')
+def effect_setting(effect_id):
+    # 1: 波紋, 2: オノマトペ
+    try:
+        _ = int(effect_id)
+    except ValueError:
+        return jsonify('idは数値する必要があります')
+
+    processingHost = "127.0.0.1"  # Processingで立ち上げたサーバのIPアドレス
+    processingPort = 10002  # Processingで設定したポート番号
+
+    name = ''
+    socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketClient.connect((processingHost, processingPort))
+
+    if effect_id == 1:
+        name = 'normal_hamon'
+    elif effect_id == 2:
+        name = 'onomatopoeia'
+
+    socketClient.send(effect_id.encode('utf-8'))
+    return jsonify({'id': id, 'name': name})
 
 
 def write_log_file(sensor, z):
